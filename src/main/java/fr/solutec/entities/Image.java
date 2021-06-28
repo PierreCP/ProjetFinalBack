2dbfc58085d0710212f40aad3c4329f3dfe0c89c
ClovisBonhomme <clovis.bonhomme@esic.fr>
2021-06-28 16:51:22 +0200

    recherche image sous blob + api correspondante (en cours)

---
+++ src/main/java/fr/solutec/entities/Image.java
@@ -0,0 +1,22 @@
+package fr.solutec.entities;
+
+import java.sql.Blob;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Data
+public class Image {
+	@Id @GeneratedValue
+	private Long id;
+	private String titre;
+	private String commentaire;
+	private Blob img;
+
+}
