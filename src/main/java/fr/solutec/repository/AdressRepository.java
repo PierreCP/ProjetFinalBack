8e3f48eb0df2a6f87c6065306ef39d7d705b48d4
gmorisse <uillaume.morisset1@gmail.com>
2021-06-29 16:57:41 +0200

    AdressRest

--- src/main/java/fr/solutec/repository/AdressRepository.java
+++ src/main/java/fr/solutec/repository/AdressRepository.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Adress;
+import fr.solutec.entities.Person;
 
 
 public interface AdressRepository extends CrudRepository<Adress, Long>{
-
+	public Optional<Adress> findById(Long id);
 }
