95e78c13276dc0aaa0d4a08a6d4dcb6cec63b39f
gmorisse <uillaume.morisset1@gmail.com>
2021-06-29 14:40:30 +0200

    modif adress

--- src/main/java/fr/solutec/entities/Adress.java
+++ src/main/java/fr/solutec/entities/Adress.java
@@ -1,6 +1,10 @@
 package fr.solutec.entities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.http.*;
@@ -11,6 +15,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,7 +26,7 @@ import lombok.NoArgsConstructor;
 public class Adress {
 	@Id @GeneratedValue
 	private Long id;
-	private int numéroRue;
+	private int numeroRue;
 	private String nomRue;
 	private int codePostal;
 	private String nomCommune;
@@ -28,28 +34,23 @@ public class Adress {
 	private double y;
 	
 	
-	public Adress(Long id, int numéroRue, String nomRue, int codePostal, String nomCommune) {
-		super();
-		this.id = id;
-		this.numéroRue = numéroRue;
-		this.nomRue = nomRue;
-		this.codePostal = codePostal;
-		this.nomCommune = nomCommune;
 
-		
-	}
 	/*
 	public double generateX() {
 		double x = 5;
 		
 		HttpClient client = HttpClient.newHttpClient();
 		var request = HttpRequest.newBuilder(
-			       URI.create("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"))
+			       URI.create("https://api-adresse.data.gouv.fr/search/?q=" + this.numéroRue + "+" + this.nomRue + "&postcode=" + this.codePostal))
 			   .header("accept", "application/json")
 			   .build();
 		
 		return x;
 	}*/
+	
+
+		
+
 	
 	
 }
