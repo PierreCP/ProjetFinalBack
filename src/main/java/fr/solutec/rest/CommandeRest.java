d9fbe023715266381551ec0b3a92144a5a283838
gmorisse <uillaume.morisset1@gmail.com>
2021-07-02 16:11:22 +0200

    recup commandes validées ou non

--- src/main/java/fr/solutec/rest/CommandeRest.java
+++ src/main/java/fr/solutec/rest/CommandeRest.java
@@ -49,4 +49,71 @@ public class CommandeRest {
 		
 		return commandes;
 	}
+	
+	
+	@GetMapping("commandes-valide/{idPers}")
+	public List<Commande> getCommandesValideById(@PathVariable Long idPers){
+		Iterable<Commande> allCommandes = commandeRepo.findAll();
+		Producteur prod = prodRepo.findByPersonId(idPers).get();
+		List<Commande> commandes = new ArrayList<Commande>();
+		
+		for (Commande commande : allCommandes) {
+			if (commande.isRecuperee()) {
+				
+			
+			Commande currentCommande = new Commande();
+			for (Produit produit : commande.contenuCommande) {
+				if (produit.getIdProd() == prod.getId()) {
+					if (currentCommande.getId() == null) {
+						currentCommande.setId(commande.getId());
+						currentCommande.setDate(commande.getDate());
+						currentCommande.setRecuperee(commande.isRecuperee());
+						currentCommande.setConsom(commande.getConsom());
+						currentCommande.setContenuCommande(new ArrayList<Produit>());
+					}
+					currentCommande.contenuCommande.add(produit);
+				}
+				
+			}
+			if (currentCommande.getId() == null) {	
+			}
+			commandes.add(currentCommande);
+		}
+		}
+		return commandes;
+	}
+	
+	
+	@GetMapping("commandes-nouvelles/{idPers}")
+	public List<Commande> getCommandesNouvellesById(@PathVariable Long idPers){
+		Iterable<Commande> allCommandes = commandeRepo.findAll();
+		Producteur prod = prodRepo.findByPersonId(idPers).get();
+		List<Commande> commandes = new ArrayList<Commande>();
+		
+		for (Commande commande : allCommandes) {
+			if (!commande.isRecuperee()) {
+				
+			
+			Commande currentCommande = new Commande();
+			for (Produit produit : commande.contenuCommande) {
+				if (produit.getIdProd() == prod.getId()) {
+					if (currentCommande.getId() == null) {
+						currentCommande.setId(commande.getId());
+						currentCommande.setDate(commande.getDate());
+						currentCommande.setRecuperee(commande.isRecuperee());
+						currentCommande.setConsom(commande.getConsom());
+						currentCommande.setContenuCommande(new ArrayList<Produit>());
+					}
+					currentCommande.contenuCommande.add(produit);
+				}
+				
+			}
+			if (currentCommande.getId() == null) {	
+			}
+			commandes.add(currentCommande);
+		}
+		}
+		return commandes;
+	}
+	
 }
