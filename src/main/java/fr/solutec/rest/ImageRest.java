8817a366b1a0f075820a05a6386d217ccec2d408
ClovisBonhomme <clovis.bonhomme@esic.fr>
2021-07-03 19:36:31 +0200

    API modif d'image produit

--- src/main/java/fr/solutec/rest/ImageRest.java
+++ src/main/java/fr/solutec/rest/ImageRest.java
@@ -7,17 +7,23 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Image;
+import fr.solutec.entities.Produit;
 import fr.solutec.repository.ImageRepository;
+import fr.solutec.repository.ProduitRepository;
 
 @RestController @CrossOrigin("*")
 public class ImageRest {
 
 	@Autowired
 	private ImageRepository ir;
+	
+	@Autowired
+	private ProduitRepository pr;
 	
 	@GetMapping("image/{id}")
 	public Optional<Image> getImage(@PathVariable Long id) {
@@ -34,4 +40,9 @@ public class ImageRest {
 		return ir.save(i);
 	}
 	
+	@PutMapping("image/{id}")
+	public Produit setImageProduit(@RequestBody byte[] img, @PathVariable Long id) {
+		pr.findById(id).get().setImage(img);
+		return pr.save(pr.findById(id).get());
+	}
 }
